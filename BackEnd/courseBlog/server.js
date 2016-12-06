3986901476074b3b5ab579b11dc21cff3d6d5804
Henry Tran <htran48@uwo.ca>
2016-12-06 14:16:03 -0500

    backend get records change

--- BackEnd/courseBlog/server.js
+++ BackEnd/courseBlog/server.js
@@ -1,21 +1,13 @@
-var bodyParser = require('body-parser')
 var express = require('express');
 var logger = require('./logger');
 var app = express();
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var studentRecord = require('./models/student.js');
 mongoose.connect('mongodb://localhost/assignment2');
 
-
-app.use(function (request, response, next) {
-    response.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    response.header('Access-Control-Allow-Methods', 'POST, PATCH, GET, PUT, DELETE, OPTIONS');
-    next();
-});
 app.use(logger);
 //app.use(express.static('public'));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
