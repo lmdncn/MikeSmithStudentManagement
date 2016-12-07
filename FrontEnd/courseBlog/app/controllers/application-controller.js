02ea677611b2da0b4898b8efc77eb90123922818
Liam Duncan <ldunca7@uwo.cagit>
2016-12-06 20:44:19 -0500

    displaying info

---
+++ FrontEnd/courseBlog/app/controllers/application-controller.js
@@ -0,0 +1,29 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+
+    //in applicationController
+doSearch: function() {
+// the current value of the text field
+	var query = this.get('search');
+	//this.transitionToRoute('search', { query: query });
+
+    // this.set('searchQuery', query);
+
+    this.transitionToRoute('home', query);
+
+},
+
+actions:{
+    search(){
+         console.log('doSearch called');
+      //How can methodB calling methodC
+      this.send('doSearch');
+      
+      return false;
+     
+    }
+
+}
+});
