9450a910bd68d0172301dafd5c86be1b72a9968c
Liam Duncan <ldunca7@uwo.cagit>
2016-12-07 01:31:14 -0500

    nav-bar component with event handler calls (only consoleLog)

--- FrontEnd/courseBlog/app/components/nav-bar.js
+++ FrontEnd/courseBlog/app/components/nav-bar.js
@@ -1,4 +1,61 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+
+
+  store: Ember.inject.service(),
+  routing: Ember.inject.service('-routing'),
+
+  actions: {
+
+  saveStudent:function(){
+    console.log("saveStudent");
+
+  
+
+  },
+
+undoChange:function(){
+    console.log("undoChange");
+
+  
+
+  },
+
+  firstRecord:function(){
+    console.log("firstRecord");
+
+  
+
+  },
+
+  prevRecord:function(){
+    console.log("prevRecord");
+
+  
+
+  },
+
+  nextRecord:function(){
+    console.log("nextRecord");
+
+  
+
+  },
+
+lastRecord:function(){
+    console.log("lastRecord");
+
+  
+
+  }
+
+
+
+  }
+
+
+
+
 });
